44a301521bcf1f02dd0f68897825fbf0c941cc4f
bianca9901 <biancawikberg99@yahoo.com>
2024-03-06 15:19:15 +0100

    Add API route for accepting friend requests

---
+++ src/app/api/friends/accept/route.ts
@@ -0,0 +1,45 @@
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+import { db } from "@/lib/db";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { id: idToAdd } = z.object({ id: z.string() }).parse(body);
+
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    const isAlreadyFriends = await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:friends`,
+      idToAdd
+    );
+    if (isAlreadyFriends) {
+      return new Response("Already freinds", { status: 400 });
+    }
+
+    const hasFriendRequest = await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:incoming_friend_requests`,
+      idToAdd
+    );
+
+    if (!hasFriendRequest) {
+      return new Response("No friend request", { status: 400 });
+    }
+
+    await db.sadd(`user:${session.user.id}:friends`, idToAdd)
+
+    await db.sadd(`user:${idToAdd}:friend`, session.user.id)
+
+    //await db.srem(`user:${idToAdd}:incoming_friend_requests`, session.user.id)
+
+    await db.srem(`user:${session.user.id};incoming_friend_requests`, idToAdd)
+
+    return new Response("OK");
+  } catch (error) {}
+}
