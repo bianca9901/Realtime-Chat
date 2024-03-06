b9a6958d103dc3c2826b1abec1d8a5c570b5165c
bianca9901 <biancawikberg99@yahoo.com>
2024-03-06 11:51:37 +0100

    Implement user profile section with image, name, email, and signout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -4,6 +4,8 @@ import { Link } from "lucide-react";
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
 import { FC, ReactNode } from "react";
+import Image from "next/image";
+import SignOutButton from "@/components/ui/SignOutButton";
 
 interface LayoutProps {
   children: ReactNode;
@@ -76,6 +78,27 @@ const Layout = async ({ children }: LayoutProps) => {
                   );
                 })}
               </ul>
+            </li>
+            <li className="-mx-6 mt-auto flex items-center">
+              <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900">
+                <div className="relative h-8 w-8 bg-gray-50">
+                  <Image
+                    fill
+                    referrerPolicy="no-referrer"
+                    className="rounded-full"
+                    src={session.user.image || ""}
+                    alt="Your profile image"
+                  />
+                </div>
+                <span className="sr-only">Your profile</span>
+                <div className="flex flex-col">
+                  <span aria-hidden="true">{session.user.name}</span>
+                  <span className="text-xs text-zinc-400" aria-hidden="true">
+                    {session.user.email}
+                  </span>
+                </div>
+              </div>
+              <SignOutButton className='h-full aspect-square' />
             </li>
           </ul>
         </nav>
