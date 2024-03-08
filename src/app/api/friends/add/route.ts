3c7ce943827e832fd86a54d766bc2725de3ec5fd
bianca9901 <biancawikberg99@yahoo.com>
2024-03-08 17:29:33 +0100

    Add functionality to send friend request notification via Pusher

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -4,6 +4,8 @@ import { addFriendValidator } from "@/lib/validations/add-friend";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 
 export async function POST(req: Request) {
   try {
@@ -53,10 +55,18 @@ export async function POST(req: Request) {
 
     // valid request, send friend request:
 
+    await pusherServer.trigger(
+      toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
+      "incoming_friend_requests",
+      {
+        senderId: session.user.id,
+        senderEmail: session.user.email,
+      }
+    );
+
     db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
 
     return new Response("OK");
-    
   } catch (error) {
     if (error instanceof z.ZodError) {
       return new Response("Invalid request payload", { status: 422 });
