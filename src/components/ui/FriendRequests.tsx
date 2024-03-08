7fc031ac2f1fb9e819ad44a6b78ca564848e582f
bianca9901 <biancawikberg99@yahoo.com>
2024-03-08 17:39:42 +0100

    Add real-time handling of incoming friend requests

--- src/components/ui/FriendRequests.tsx
+++ src/components/ui/FriendRequests.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { pusherClient } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import axios from "axios";
 import { Check, UserPlus, X } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 
 interface FriendRequestsProps {
   incomingFriendRequests: IncomingFriendRequest[];
@@ -17,6 +19,29 @@ const FriendRequests: FC<FriendRequestsProps> = ({
   const [friendRequests, setFriendRequests] = useState<IncomingFriendRequest[]>(
     incomingFriendRequests
   );
+
+  useEffect(() => {
+    pusherClient.subscribe(
+      toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+    );
+
+    const friendRequestHandler = ({
+      senderId,
+      senderEmail,
+    }: IncomingFriendRequest) => {
+      setFriendRequests((prev) => [...prev, { senderId, senderEmail }]);
+    };
+
+    pusherClient.bind("incoming_friend_requests", friendRequestHandler);
+
+    return () => {
+      pusherClient.unsubscribe(
+        toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+      );
+
+      pusherClient.unbind("incoming_friend_requests", friendRequestHandler);
+    };
+  }, [sessionId]);
 
   const acceptFriend = async (senderId: string) => {
     await axios.post("/api/friends/accept", { id: senderId });
@@ -44,7 +69,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
             <UserPlus className="text-black" />
             <p className="font-medium text-lg">{request.senderEmail}</p>
             <button
-            onClick={() => acceptFriend(request.senderId)}
+              onClick={() => acceptFriend(request.senderId)}
               aria-label="accept friend"
               className="w-8 h-8 bg-indigo-600
          hover:bg-indigo-700 grid place-items-center rounded-full transition
@@ -54,7 +79,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
             </button>
 
             <button
-            onClick={() => denyFriend(request.senderId)}
+              onClick={() => denyFriend(request.senderId)}
               aria-label="deny friend"
               className="w-8 h-8 bg-red-600
          hover:bg-red-700 grid place-items-center rounded-full transition
