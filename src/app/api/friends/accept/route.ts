a6cdeef84c080965d7e297a43c1936f10a429402
bianca9901 <biancawikberg99@yahoo.com>
2024-03-09 10:31:16 +0100

    Fix bug so that user gets realtime notification when a friend is added

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -3,6 +3,8 @@ import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 
 export async function POST(req: Request) {
   try {
@@ -32,13 +34,31 @@ export async function POST(req: Request) {
       return new Response("No friend request", { status: 400 });
     }
 
-    await db.sadd(`user:${session.user.id}:friends`, idToAdd);
+    const [userRaw, friendRaw] = (await Promise.all([
+      fetchRedis("get", `user:${session.user.id}`),
+      fetchRedis("get", `user:${idToAdd}`),
+    ])) as [string, string];
 
-    await db.sadd(`user:${idToAdd}:friends`, session.user.id);
+    const user = JSON.parse(userRaw) as User;
+    const friend = JSON.parse(friendRaw) as User;
 
-    //await db.srem(`user:${idToAdd}:incoming_friend_requests`, session.user.id)
+    // notify added user
 
-    await db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd);
+    await Promise.all([
+      pusherServer.trigger(
+        toPusherKey(`user:${idToAdd}:friends`),
+        "new_friend",
+        user
+      ),
+      pusherServer.trigger(
+        toPusherKey(`user:${session.user.id}:friends`),
+        "new_friend",
+        friend
+      ),
+      db.sadd(`user:${session.user.id}:friends`, idToAdd),
+      db.sadd(`user:${idToAdd}:friends`, session.user.id),
+      db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd),
+    ]);
 
     return new Response("OK");
   } catch (error) {
